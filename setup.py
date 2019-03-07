e986e14fadedb5364d415f2d16c83885482726c0
Kype <shichiromail@gmail.com>
2019-03-07 08:44:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,30 @@
 
 ### Put your code below this comment ###
 import sys
+from ethWrapper import ContractWrapper
+
+
+
+# === Init === #
+
+
+try:
+    GAS_PRICE = int(getData('https://gasprice.poa.network').json()['fast'] * 1000000000)
+except:
+    GAS_PRICE = 0
+
+
 
 # === Commands === #
 
 def deploy():
-    
+    pass    
 
 
 commands = {
     'deploy': deploy
 }
+
 
 
 # === Entry point === # 
