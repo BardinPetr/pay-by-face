5894eeb030c5ed86276a83773dcd5f72445cb7d6
Shichiro <shichiromail@gmail.com>
2019-03-08 16:13:57 +0300

    test list-del/add

--- kyc.py
+++ kyc.py
@@ -27,9 +27,18 @@ def list(list_name):
         if addlist_len == 0:
             print('No KYC registration requests found')
         else:
+            addlist = {}
+            phones_list = []
             for i in range(addlist_len):
                 elem = registrar.addWaitingPhones(i)
-                print('{}: {}'.format(elem[0], elem[1]))
+                phones_list.append(elem[1])
+                addlist[elem[1]] = elem[0]
+
+            phones_list.sort()
+
+            for phone in phones_list:
+                addr = addlist[phone]
+                print('{}: {}'.format(phone, addr))
 
     elif list_name == 'del':
         dltlist_len = registrar.getWaitingDeletionCnt()
@@ -37,9 +46,18 @@ def list(list_name):
         if dltlist_len == 0:
             print('No KYC unregistration requests found')
         else:
+            dltlist = {}
+            phones_list = []
             for i in range(dltlist_len):
                 elem = registrar.delWaitingPhones(i)
-                print('{}: {}'.format(elem[0], elem[1]))
+                phones_list.append(elem[1])
+                addlist[elem[1]] = elem[0]
+
+            phones_list.sort()
+
+            for phone in phones_list:
+                addr = addlist[phone]
+                print('{}: {}'.format(phone, addr))
 
 
 commands = {
