3b43ee8c3aa62b7f9d18fbed417d6017587464ab
Shichiro <shichiromail@gmail.com>
2019-03-08 11:54:55 +0300

    first init

--- kyc.py
+++ kyc.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
 
 ### Put your code below this comment ###
-print("Show must go on")
+import sys
+from requests import get as getData
+from ethWrapper import ContractWrapper
+import ethWrapper
+from tools import parceJson, toAddress
+from web3 import Web3, HTTPProvider
+from json import dump
+
+
+# === Init === #
+
+
+
+
+# === Commands === #
+def get(phone_number):
+    pass
+
+commands = {
+    'get': get
+}
+
+
+# === Entry point === #
+if __name__ == '__main__':
+    commands[sys.argv[1][2:]](*sys.argv[2:])
