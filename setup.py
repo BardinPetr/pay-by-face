67efd1227d9d2c4c042128e04bde387f47995900
Bardin Petr <bardin.petr@gmail.com>
2019-03-10 14:15:13 +0300

    Payment contract & minor fixes

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 ### Put your code below this comment ###
 import sys
+from time import sleep
+
 from ethWrapper import ContractWrapper
 from json import dump
 from network import *
@@ -9,20 +11,28 @@ from network import *
 
 # === Commands === #
 def deploy():
-    registrar = ContractWrapper(w3=web3, abi=registrar_ABI, bytecode=registrar_BYTECODE)
-    payment = ContractWrapper(w3=web3, abi=payment_ABI, bytecode=payment_BYTECODE)
+    for i in range(20):
+        try:
+            registrar = ContractWrapper(w3=web3, abi=registrar_ABI, bytecode=registrar_BYTECODE)
+            payment = ContractWrapper(w3=web3, abi=payment_ABI, bytecode=payment_BYTECODE)
 
-    registrar_rcpt = registrar.constructor()
-    payment_rcpt = payment.constructor()
+            registrar_rcpt = registrar.constructor()
+            payment_rcpt = payment.constructor()
 
-    with open('registrar.json', 'w') as f:
-        dump({
-            'registrar': {'address': registrar_rcpt['contractAddress'], 'startBlock': registrar_rcpt['blockNumber']},
-            'payments': {'address': payment_rcpt['contractAddress'], 'startBlock': payment_rcpt['blockNumber']}
-        }, f)
+            with open('registrar.json', 'w') as f:
+                dump({
+                    'registrar': {'address': registrar_rcpt['contractAddress'],
+                                  'startBlock': registrar_rcpt['blockNumber']},
+                    'payments': {'address': payment_rcpt['contractAddress'], 'startBlock': payment_rcpt['blockNumber']}
+                }, f)
 
-    print('KYC Registrar: ' + registrar_rcpt['contractAddress'])
-    print('Payment Handler: ' + payment_rcpt['contractAddress'])
+            print('KYC Registrar: ' + registrar_rcpt['contractAddress'])
+            print('Payment Handler: ' + payment_rcpt['contractAddress'])
+            break
+        except Exception as ex:
+            if str(ex).find('Insufficient funds') != -1:
+                raise Exception('Low balance')
+            sleep(8)
 
 
 def owner(contract_name):
